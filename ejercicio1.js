4ef271e4117665f6b45a21a5449c565818f0da42
vanessaospina <vanessahuila48@gmail.com>
2022-06-24 21:53:51 -0500

    segundo commit

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,9 +1,20 @@
+const _private=new WeakMap();
 class libros{
     constructor(autor,nombre,numeropaginas){
-    this.autor=autor
-    this.nombre=nombre
-    this.numeropaginas=numeropaginas  
+        const propiedades={
+        _autor:autor,
+        _nombre:nombre,
+        _numeropaginas:numeropaginas     
+        }
+        _private.set(this,{propiedades})
     }
+    set nombrelibro(nombre1){
+        return _private.get(this).propiedades["_nombre"]=nombre1
+    }
+    get mostrarlibro(){
+        return _private.get(this).propiedades["_nombre"];
+    }
+
     
 }
 let libro1=new libros("gabriel garcia marquez","cienn años de soledad","350")
