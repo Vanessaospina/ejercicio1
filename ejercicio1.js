de0984367fb544acb5e3e5be35344d2ee707dfd0
vanessaospina <vanessahuila48@gmail.com>
2022-06-24 20:44:35 -0500

    mi primer comit

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+class libros{
+    constructor(autor,nombre,numeropaginas){
+    this.autor=autor
+    this.nombre=nombre
+    this.numeropaginas=numeropaginas  
+    }
+    
+}
+let libro1=new libros("gabriel garcia marquez","cienn años de soledad","350")
+console.log(libro1)
